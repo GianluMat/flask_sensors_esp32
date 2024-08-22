993ddb6a9e2f7b740a07a63a08e9c239efaf9d90
GianluMat <gianluca.mattiello89@gmail.com>
2024-08-22 23:07:05 +0200

    aggiunto pattern singleton per la connessione al db

--- app.py
+++ app.py
@@ -5,16 +5,23 @@ import os
 
 app = Flask(__name__)
 
-mongo_uri = "mongodb+srv://GianITS:ProjectITS33@clusterits.do6lt.mongodb.net/?retryWrites=true&w=majority&appName=ClusterITS"
-client = MongoClient(mongo_uri)
-db = client["sensors_db"]
-collection = db["sensors_data"]
+client = None
+
+def get_db():
+    global client
+    if client is None:
+        # Crea l'istanza di MongoClient solo se non esiste ancora
+        mongo_uri = "mongodb+srv://GianITS:ProjectITS33@clusterits.do6lt.mongodb.net/?retryWrites=true&w=majority&appName=ClusterITS"
+        client = MongoClient(mongo_uri)
+    return client["sensors_db"]
 
 @app.route('/api/sensordata', methods=['POST'])
 def receive_sensor_data():
     try:
         data = request.json
         data['timestamp'] = datetime.now(datetime.UTC)
+        db = get_db()
+        collection = db["sensors_data"]
         collection.insert_one(data)
         
         return jsonify({"message": "Data saved successfully"}), 201
@@ -24,6 +31,8 @@ def receive_sensor_data():
 @app.route('/api/sensordata', methods=['GET'])
 def get_sensor_data():
     try:
+        db = get_db()
+        collection = db["sensors_data"]
         data = list(collection.find({}, {"_id": 0}))
         return jsonify(data), 200
     except Exception as e:
